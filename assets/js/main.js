18f72efba30834e6672d3a15c8fb3acfda455d19
Shahriar Sohag <shahriarr27@gmail.com>
2022-12-25 14:21:43 +0600

    responsive

--- assets/js/main.js
+++ assets/js/main.js
@@ -78,28 +78,6 @@ $( document ).ready(function() {
       freeModeMomentum: false
     });
 
-//start rating
-// var $star_rating = $('.star-rating .bi');
-
-// var SetRatingStar = function() {
-//   return $star_rating.each(function() {
-//     if (parseInt($star_rating.siblings('input.rating-value').val()) >= parseInt($(this).data('rating'))) {
-//       return $(this).removeClass('bi-star').addClass('bi-star-fill');
-//     } else {
-//       return $(this).removeClass('bi-star-fill').addClass('bi-star');
-//     }
-//   });
-// };
-
-// $star_rating.on('click', function() {
-//   $star_rating.siblings('input.rating-value').val($(this).data('rating'));
-//   return SetRatingStar();
-// });
-
-// SetRatingStar();
-// $(document).ready(function() {
-
-// });
 
 const starBtn = $('.rating input');
 const formFields = $('.form-group-hidden');
